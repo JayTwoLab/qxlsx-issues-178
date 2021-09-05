2727c04aa44b3f5828e84735ab9946528fa3751c
Jay Two <j2doll@gmail.com>
2021-09-05 21:37:04 +0900

    update code

--- issue178.pro
+++ issue178.pro
@@ -1,4 +1,4 @@
-QT -= gui
+QT += core
 
 CONFIG += c++11 console
 CONFIG -= app_bundle
@@ -7,6 +7,14 @@ CONFIG -= app_bundle
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+##########################################################################
+# NOTE: You can fix value of QXlsx path of source code.
+QXLSX_PARENTPATH=../QXlsx/QXlsx
+QXLSX_HEADERPATH=../QXlsx/QXlsx/header/
+QXLSX_SOURCEPATH=../QXlsx/QXlsx/source/
+include(../QXlsx/QXlsx/QXlsx.pri)
+
+
 SOURCES += \
         main.cpp
 
@@ -14,3 +22,4 @@ SOURCES += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
--- main.cpp
+++ main.cpp
@@ -1,8 +1,43 @@
+// main.cpp
+
+#include <QtGlobal>
+#include <QDebug>
 #include <QCoreApplication>
+
+// [0] include QXlsx headers
+#include "xlsxdocument.h"
+#include "xlsxchartsheet.h"
+#include "xlsxcellrange.h"
+#include "xlsxchart.h"
+#include "xlsxrichstring.h"
+#include "xlsxworkbook.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    return a.exec();
+    using namespace QXlsx;
+
+    Document test("test.xlsx");
+
+    test.addSheet();
+
+    for ( int numero = 0 ; numero < 10 ; numero++ )
+    {
+        float x = numero / 2. ;
+        float y = numero * numero ;
+
+        Format format_nombre;
+        test.write( numero, 1, x, format_nombre );
+        test.write( numero, 2, y, format_nombre );
+    }
+
+    Chart* chart = test.insertChart( 9, 1, QSize(400, 400) );
+    chart->setChartType( Chart::CT_ScatterChart );
+
+    chart->addSeries( CellRange("A1:B9") );
+
+    test.save();
+
+    return 0; // return a.exec();
 }
